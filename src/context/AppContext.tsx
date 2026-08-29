87150fa58be766ffdbe3a57e2231c976bddce350
Docente <docente@scuola.it>
2026-08-29 09:57:41 +0200

    Allineamento esatto Lunedi MARCHI e PELLEGRINO: MARCHI 6a in 3B, 8a in 2F con PELLEGRINO (ALTERNATIVA) in compresenza perfetta

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -3,7 +3,7 @@ import { Docente, OrarioDocente, AssenzaDocente, UscitaClasse, SostituzioneAsseg
 import { DOCENTI_PRECARICATI, ORARI_DOCENTI_PRECARICATI } from '../data/initialData';
 import { getDocentiCollegatiIds, getOrarioUnificatoDocente, getBaseNomeDocente } from '../utils/docentiHelper';
 
-const CURRENT_TIMETABLE_VERSION = 'v16_pdf_marchi_6th_hour_fix';
+const CURRENT_TIMETABLE_VERSION = 'v17_pdf_marchi_pellegrino_compresenza_fix';
 
 export const DEFAULT_IMPOSTAZIONI_SCUOLA: ImpostazioniScuola = {
   nomeScuola: 'I.C. Leonardo da Vinci',
