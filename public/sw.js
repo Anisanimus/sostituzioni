288ae1e00c72bbf629f4c8f50dcc49edf8466c37
Docente <docente@scuola.it>
2026-09-02 15:29:52 +0200

    fix(cache): add aggressive cache-busting headers for iOS/Safari and update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,15 @@
-﻿self.addEventListener('install', (event) => {
+const SW_VERSION = 'sw_v_1725283750';
+
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('notificationclick', (event) => {
